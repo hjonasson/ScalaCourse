f0ec1c5783dd0f72f980e7cfe3c01d8167e200f4
Hjörtur Jónasson <hjortur.jonasson@kaust.edu.sa>
2014-07-16 23:26:13 +1000

    Solved 1.2

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -1,5 +1,4 @@
 package recfun
-import common._
 
 object Main {
   def main(args: Array[String]) {
@@ -9,20 +8,35 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
-  }
+  }                                               //> main: (args: Array[String])Unit
 
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int =
     if (c == 0) 1 else if (r == 0) 0 else pascal(c-1,r-1) + pascal(c,r-1)
+                                                  //> pascal: (c: Int, r: Int)Int
+  pascal(2,3)                                     //> res0: Int = 3
+  
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
-
+  def balance(chars: List[Char]): Boolean = {
+    if (chars.isEmpty) true
+    if (chars.head ==')') false
+    def counter(c: List[Char],acc: Int): Int =
+      if (c isEmpty) acc
+      else if (c.head == '(') counter(c.tail,acc+1)
+      else if (c.head == ')') counter(c.tail,acc-1)
+      else counter (c.tail,acc)
+    counter(chars,0) == 0
+  }                                               //: (chars: List[Char])Boolean
+                                                  //> balance: (chars: List[Char])Boolean
+  balance(List('(',')'))                          //> res1: Boolean = true
+  balance(List('(','1'))                    //> res2: Boolean = false
   /**
    * Exercise 3
    */
   def countChange(money: Int, coins: List[Int]): Int = ???
+                                                  //> countChange: (money: Int, coins: List[Int])Int
 }
